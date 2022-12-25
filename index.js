051e9a9bb55b49dcf7e7430e0565a6519651306e
Anton Pryakhin <dr.pryah@gmail.com>
2022-12-25 18:03:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import colors from "colors";
+import readline from "readline";
+import path from "path";
+import inquirer from "inquirer";
+import fsp from "fs/promises";
+
+console.log(colors.red("Hello World!"));
